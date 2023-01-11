d9e864a632c52e591514cfa48b9f12a6dd781a06
Nathan Lefetey <lefetey.nathan@gmail.com>
2023-01-11 19:18:26 +0100

    :hammer: ADD fake connexion and local storage for team

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-for (let i = 0; i < 6; i++) {
-    let pokeId = Math.floor(Math.random() * 1154)
-    fetch('https://pokeapi.co/api/v2/pokemon' + pokeId)
-        .then(result => result.json())
-        .then(data => {
-            
-        })
+let teamForm = document.getElementById('team-name-form')
+let field = document.getElementById('team-name-input')
+let fieldSubmit = document.getElementById('team-name-submit')
+let currentUser
+let myTeam = document.getElementById('my-team')
+let username = document.createElement('h1')
+let myPokedex = document.createElement('div')
+
+class Team
+{
+    constructor(username, pokeList)
+    {
+        this.username = username;
+        this.pokeList = pokeList
+    }
+}
+
+fieldSubmit.onclick = (e) => {
+    e.preventDefault()
+    currentUser = field.value
+    teamForm.style.display = 'none'
+    username.innerHTML = currentUser
+    myTeam.appendChild(username)
+}
+
+let pokeId = [1, 246, 789, 564, 321, 665]
+
+
+
+let validation = document.getElementById('validation')
+
+validation.onclick = () => {
+    let thisTeam = new  Team(currentUser, pokeId)
+    console.log(currentUser)
+    console.log(thisTeam)
+    localStorage.setItem(currentUser, JSON.stringify(thisTeam))
 }
