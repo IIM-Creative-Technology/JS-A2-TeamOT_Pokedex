8a422f6de9609b1db9123b7d9d3f61d9f3fff678
MortyOW <adam.simon@edu.devinci.fr>
2023-01-15 13:45:24 +0100

    :hammer: Username added

--- login.js
+++ login.js
@@ -6,21 +6,25 @@
     const signupForm = document.getElementById("signup-form");
     signupForm.addEventListener("submit", event => {
     event.preventDefault();
+    const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    users[email] = password;
+    users[email] = {username:username,password:password};
     localStorage.setItem("users", JSON.stringify(users));
     console.log("Sign up successful!");
     });
 
+
+    
     const loginForm = document.getElementById("login-form");
     loginForm.addEventListener("submit", event => {
     event.preventDefault();
+    const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    if(users[email] === password){
-        alert('You are logged in')
+    if(users[email] && users[email].username === username && users[email].password === password){
+        console.log("Logged in successfully!");
     } else {
-        alert('Invalid email or password !')
+        console.log("Invalid username, email or password!");
     }
-    });
+});
