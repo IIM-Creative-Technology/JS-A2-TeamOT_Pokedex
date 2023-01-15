6de1a3af8f4c29a973f97a2117f268361fda670f
MortyOW <adam.simon@edu.devinci.fr>
2023-01-15 14:52:23 +0100

    :sparkles: User feature finished

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
-    let users = {};
+
+    let users = [];
     if(localStorage.getItem("users")){
     users = JSON.parse(localStorage.getItem("users"));
     }
@@ -9,22 +10,28 @@
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    users[email] = {username:username,password:password};
+    users.push({username:username,email:email,password:password});
     localStorage.setItem("users", JSON.stringify(users));
     console.log("Sign up successful!");
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
     });
 
 
-    
+
     const loginForm = document.getElementById("login-form");
     loginForm.addEventListener("submit", event => {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    if(users[email] && users[email].username === username && users[email].password === password){
-        console.log("Logged in successfully!");
-    } else {
-        console.log("Invalid username, email or password!");
+    const email = document.getElementById("email2").value;
+    const password = document.getElementById("password2").value;
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i].email)
+        if(users[i].email == email && users[i].password == password){
+            localStorage.setItem("currentUser", JSON.stringify(users[i]))
+            window.location = "index.html"
+        }else{
+            i++
+        }
     }
 });
